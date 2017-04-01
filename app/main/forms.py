d4ae7515359f162b792a9c42f2870872baaf9a39
ZYN <1474349390@qq.com>
2017-04-01 23:03:46 +0800

    Delete 5

--- app/main/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SelectField, SubmitField,TextAreaField
-from wtforms.validators import Required, Length
-
-class QueryForm(FlaskForm):
-    select = SelectField('按房间/日期查询', validators=[Required()], coerce=int,choices=[])
-    submit = SubmitField('确认')
